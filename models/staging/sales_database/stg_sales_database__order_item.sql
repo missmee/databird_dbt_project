2f6c3a730cf6fbbd0f3a868d2e02c9dfa4b34121
missmee <missmee@free.fr>
2024-11-19 16:21:42 +0100

    fix: adding suffix _y to column names

--- models/staging/sales_database/stg_sales_database__order_item.sql
+++ models/staging/sales_database/stg_sales_database__order_item.sql
@@ -1,11 +1,11 @@
-select CONCAT(order_id, '_', product_id) AS order_item_id_x,
+select CONCAT(order_id, '_', product_id) AS order_item_id_y,
  order_id,
  product_id,
- 1 AS column_1_x,
+ 1 AS column_1_y,
  seller_id,
- DATETIME(pickup_limit_date, "Europe/Paris") AS picked_up_limited_at_x,
- price AS unit_price_x,
+ DATETIME(pickup_limit_date, "Europe/Paris") AS picked_up_limited_at_y,
+ price AS unit_price_y,
  shipping_cost,
- quantity AS item_quantity_x,
- (price * quantity) + shipping_cost AS total_order_item_amount_x
+ quantity AS item_quantity_y,
+ (price * quantity) + shipping_cost AS total_order_item_amount_y
 from {{ source('sales_database', 'order_item') }}
