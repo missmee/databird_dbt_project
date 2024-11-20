c305dc15d17ce4b406b3f66973030d8fd5bfa5f8
jmissmee@gmail.com <jmissmee@gmail.com>
2024-11-20 08:12:51 +0000

    fix: saved

--- models/staging/sales_database/stg_sales_database__order_item.sql
+++ models/staging/sales_database/stg_sales_database__order_item.sql
@@ -6,11 +6,6 @@ select CONCAT(order_id, '_', product_id) AS order_item_id_y,
  DATETIME(pickup_limit_date, "Europe/Paris") AS picked_up_limited_at_y,
  price AS unit_price_y,
  shipping_cost,
-<<<<<<< HEAD
- --quantity as item_quantity,
- (price * quantity) + shipping_cost as total_order_item_amount
-=======
- quantity AS item_quantity_y,
+  quantity AS item_quantity_y,
  (price * quantity) + shipping_cost AS total_order_item_amount_y
->>>>>>> 337a710332a74ad395ab4374bdad6ac65d0228d0
 from {{ source('sales_database', 'order_item') }}
